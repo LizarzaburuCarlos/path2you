ea96fe78b7a6fb0bc29c44f0f2e7b84ea04ac5fd
Carlos Lizarzaburu <carlos_abanto2@hotmail.com>
2023-10-03 12:50:50 -0500

    news structure

--- path2you-frontend/src/components/home/News.tsx
+++ path2you-frontend/src/components/home/News.tsx
@@ -2,8 +2,15 @@
 
 const News = () => {
     return (
-        <section className="w-full h-fit bg-blue">
-
+        <section className="news w-full h-72 bg-blue">
+            <div className="news__container h-full w-full flex  flex-wrap gap-4">
+                <div className="img1 rounded-xl bg-slate-200 h-full w-1/2 md:w-1/3 flex-1 order-1"></div>
+                <div className="news__medium flex flex-col gap-4 h-full w-full md:w-1/3 md:flex-1 order-2 max-md:order-3">
+                    <div className="img2 rounded-xl bg-slate-200 basis-1/2"></div>
+                    <div className="img3 rounded-xl bg-slate-200 basis-1/2"></div>
+                </div>
+                <div className="img3 rounded-xl bg-slate-200 h-full w-1/2 md:w-1/3 flex-1 order-3 max-md:order-2"></div>
+            </div>
         </section>
     )
 }
