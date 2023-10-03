e02730954e76bcfc4636dde8374ea329a1e0d9b3
Carlos Lizarzaburu <carlos_abanto2@hotmail.com>
2023-10-03 14:51:52 -0500

    topbar structure

--- path2you-frontend/src/components/TopBar.tsx
+++ path2you-frontend/src/components/TopBar.tsx
@@ -1,1 +1,18 @@
 
+const TopBar = () => {
+
+    return(
+        <section className="topbar w-full h-16 mb-12 flex items-center justify-between">
+            <form className="topbar__searchbar">
+                <input type="text" placeholder="Buscar" className="topbar__search-input"/>
+                <button type="submit" className="topbar__search-button">L</button>
+            </form>
+            <div className="topbar__side-buttons">
+                <button className="topbar__button">M</button>
+                <button className="topbar__button">N</button>
+            </div>
+        </section>
+    )
+}
+
+export default TopBar;
