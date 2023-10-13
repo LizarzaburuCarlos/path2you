0080cecdb422d90746e037daad7e1c1c95cdcecb
Camila Rivera Calvo <camila.arc91@gmail.com>
2023-10-12 22:47:17 -0500

    baselayout slot styles

--- path2you-frontend/src/components/InscribedList.tsx
+++ path2you-frontend/src/components/InscribedList.tsx
@@ -3,6 +3,7 @@ import type Course from "../interfaces/course";
 import { userData } from "../core/helpers";
 import fetchApi from "../lib/strapi";
 import type Inscription from "../interfaces/inscription";
+import type User from "../interfaces/user";
 
 //FALTA:
 // 1. Poner un loading de carga para los cursos
@@ -11,6 +12,7 @@ import type Inscription from "../interfaces/inscription";
 const InscribedList = () => {
   const [inscribedCourses, setInscribedCourses] = useState<Inscription[]>([]);
   const [user, setUser] = useState({ id: null });
+  const [style, setStyle] = useState<string>("");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -31,20 +33,45 @@ const InscribedList = () => {
           "filters[user][id][$eq]": user.id,
         },
       });
-
+      fetchUserData(user.id);
       setInscribedCourses(inscribedCoursesData);
     } catch (error) {
       console.log("error", error);
     }
   }
 
+  async function fetchUserData(user) {
+    try {
+      const userDataApi = await fetchApi<User>({
+        endpoint: "users/" + user,
+      });    
+      fetchStyle(userDataApi);
+      // console.log(userDataApi);
+
+    } catch (error) {
+      console.log("error", error);
+    }
+}
+
+const fetchStyle = async (usuario) => {
+
+  if (usuario!.darkmode === true) {
+    setStyle("dark");
+  } else if (usuario!.neumorphismmode === true) {
+    setStyle("neumorphism");
+  } else {
+    setStyle("light");
+  }
+
+};
+
   const InscribedCourse = (inscription: Inscription) => {
     const course = inscription.attributes.course.data.attributes;
     const inscriptionData = inscription.attributes;
     return (
-      <a href={`/courses/${course.slug}`} className="inscribedcourse w-full h-24 py-4 rounded-lg bg-slate-200 flex items-center gap-6">
-        <div className="inscribedcourse__presentation w-6/12 pl-6 flex items-center gap-4">
-          <div className="img h-12 w-12 bg-black"></div>
+      <a href={`/courses/${course.slug}`} className={`inscribedcourse ${style} w-full h-20 py-4 rounded-lg flex items-center gap-6`}>
+        <div className="inscribedcourse__presentation w-6/12 pl-6 flex items-center gap-6">
+          <div className="img h-12 w-12 rounded-lg "></div>
           <h4>{course.title}</h4>
         </div>
         <p className="inscribedcourse__date w-6/12 pr-6">{inscriptionData.date.toString()}</p>
@@ -57,8 +84,8 @@ const InscribedList = () => {
       <div className="inscribedlist__container">
         {inscribedCourses.length > 0 ? (
           <>
-            <div className="inscribedlist__header flex items-center gap-6 mb-4">
-              <h3 className="w-6/12 font-semibold text-customText">
+            <div className={`inscribedlist__header ${style} flex items-center gap-6 mb-6`}>
+              <h3 className="w-6/12 font-semibold ">
                 Cursos del usuario:
               </h3>
               <h3 className="w-6/12">Fecha de inicio:</h3>
