dfc5696b4494bf8f21411e348ee4d9cbcefad4bc
Carlos Lizarzaburu <carlos_abanto2@hotmail.com>
2023-10-17 23:53:13 -0500

    strapi fix

--- path2you-strapi/config/database.js
+++ path2you-strapi/config/database.js
@@ -64,7 +64,7 @@ module.exports = ({ env }) => {
         ssl: env.bool("DATABASE_SSL", false) && {
           key: env("DATABASE_SSL_KEY", undefined),
           cert: env("DATABASE_SSL_CERT", undefined),
-          ca: env("DATABASE_SSL_CA", undefined),
+          ca: env("CA_CERT"),
           capath: env("DATABASE_SSL_CAPATH", undefined),
           cipher: env("DATABASE_SSL_CIPHER", undefined),
           rejectUnauthorized: env.bool(
