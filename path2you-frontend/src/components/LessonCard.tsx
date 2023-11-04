c18faf90f06442f78ec85a591e868ea2419cded6
Camila Rivera Calvo <camila.arc91@gmail.com>
2023-11-04 15:26:58 -0500

    oncheck lessoncard finished

--- path2you-frontend/src/components/LessonCard.tsx
+++ path2you-frontend/src/components/LessonCard.tsx
@@ -5,15 +5,34 @@ import type User from "../interfaces/user";
 import { userData } from "../core/helpers";
 import { getUsuario } from "../core/service";
 import "../styles/LectureCardDefault.styles.css";
+import fetchApi from "../lib/strapi";
+import type Progress from "../interfaces/progress";
 
 const LessonCard = ({ lesson, setResolution, setLeccion }) => {
+
     const [hasMedia, setHasMedia] = useState<boolean>(false);
+    const [isFinished, setIsFinished] = useState<boolean>(false);
   
     useEffect(() => {
 
       if (lesson.attributes.media.data) {
         setHasMedia(true);
       }
+
+      const checkProgress = async () => {
+        const usuario = await userData();
+        const query = `filters[user][id][$eq]=${usuario.id}&filters[lesson][id][$eq]=${lesson.id}`
+        const progressData = await fetchApi<Progress>({
+        endpoint: "progresses?"+query,
+        wrappedByKey: "data",
+      });
+      
+        if (progressData[0].attributes.finished === true) {
+          setIsFinished(true);
+        }
+    };
+
+    checkProgress();
       
       
     }, []);
@@ -24,7 +43,7 @@ const LessonCard = ({ lesson, setResolution, setLeccion }) => {
     }
   
     return (
-        <button onClick={handleLesson} className={`lecturecard w-full md:w-[60%] mx-4 h-24 px-6 py-6 flex gap-6 items-center`}>
+        <button onClick={handleLesson} className={`lecturecard w-full md:w-[70%] mx-4 h-24 px-6 py-6 flex gap-6 items-center`}>
             <div className={`lecturecard__media w-[15%] lg:w-[10%] flex justify-start items-center`}>
                 {hasMedia === true && (
                     <div className="h-10 w-10 md:h-14 md:w-14 flex justify-center items-center">
@@ -44,6 +63,18 @@ const LessonCard = ({ lesson, setResolution, setLeccion }) => {
                         {lesson.attributes.title}
                     </h3>
                 </div>
+            </div>
+            <div className={`lecturecard__status w-[15%] lg:w-[10%] flex justify-end items-center`}>
+                {isFinished === true && (
+                    <div className="h-10 w-10 md:h-14 md:w-14 flex justify-center items-center">
+                        <i className="fa-solid fa-check mx-auto text-lg md:text-xl"></i>
+                    </div>
+                )}
+                {isFinished === false && (
+                    <div className="h-10 w-10 md:h-14 md:w-14 flex justify-center items-center">
+                        <i className="fa-solid fa-circle mx-auto text-lg md:text-xl"></i>
+                    </div>
+                )}
             </div>
       </button>
     );
