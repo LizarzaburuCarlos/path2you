f528578a3c83f7d5bb0666f48cff0eaac3ea6ce9
Camila Rivera Calvo <camila.arc91@gmail.com>
2023-11-07 18:02:25 -0500

    Revert "new interfaces"
    This reverts commit 068b056ab7aabff90b56cd11253b4c05824c1c8c.

--- path2you-frontend/src/core/constants.ts
+++ path2you-frontend/src/core/constants.ts
@@ -1,4 +1,4 @@
-// export const API_URL = "https://digital-ocean-strapi-ylsgi.ondigitalocean.app/api"
-// export const STRAPI_URL = "https://digital-ocean-strapi-ylsgi.ondigitalocean.app"
-export const API_URL = "http://localhost:1337/api"
-export const STRAPI_URL = "http://localhost:1337"
+export const API_URL = "https://digital-ocean-strapi-ylsgi.ondigitalocean.app/api"
+export const STRAPI_URL = "https://digital-ocean-strapi-ylsgi.ondigitalocean.app"
+// export const API_URL = "http://localhost:1337/api"
+// export const STRAPI_URL = "http://localhost:1337"
