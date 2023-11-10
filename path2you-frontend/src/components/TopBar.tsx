242dbc006b5d9fbcc1c324326b4eb00e1f3a60de
Carlos Lizarzaburu <carlos_abanto2@hotmail.com>
2023-11-10 15:48:49 -0500

    home responsive & toggle topbar implementation

--- path2you-frontend/src/components/TopBar.tsx
+++ path2you-frontend/src/components/TopBar.tsx
@@ -6,61 +6,62 @@ import type User from "../interfaces/user";
 import { toast } from "react-toastify";
 
 const TopBar = () => {
+  const [usser, setUsser] = useState<User>();
+  const [light, setLight] = useState<boolean>();
+  const [dark, setDark] = useState<boolean>();
+  const [neumorphism, setNeumorphism] = useState<boolean>();
+  const [isOpen, setIsOpen] = useState(false);
 
-    const [usser, setUsser] = useState<User>();
-    const [light, setLight] = useState<boolean>();
-    const [dark, setDark] = useState<boolean>();
-    const [neumorphism, setNeumorphism] = useState<boolean>();
+  const logout = () => {
+    localStorage.setItem("user", "");
+    location.replace("/");
+  };
 
-    useEffect(() => {
-      
-        const fetchData = async () => {
-          const userDataResponse = await userData();
-          const usuarioCompleto = await fetchUserData(userDataResponse.id);
-          setUsser(usuarioCompleto);
-         
-        };
-        
-        fetchData();
-      }, []);
-
-      useEffect(() => {
-        handleFirstActive(usser);
-       
-      }, [usser]);
-
-    async function fetchUserData(user) {
-        try {
-          const userDataApi = await fetchApi<User>({
-            endpoint: "users/" + user,
-          });
-          
-          return userDataApi;              
-        } catch (error) {
-          console.log("error", error);
-        }
-    }
-
-    const editStyleUsuario = async (dark: boolean, neu: boolean) => {
-      const usuarioUpdated = { ...usser!, darkmode: dark, neumorphismmode: neu };
-      // console.log(usuarioUpdated);
-      try {
-        const res = await editarUsuario(usuarioUpdated.id, usuarioUpdated);
-        storeUser(res);
-        console.log(res);
-        if (!res.status) {
-          setUsser(usuarioUpdated);
-          
-        } else {
-          toast.error(res);
-          console.log(res);
-        }
-      } catch (error) {
-        toast.error("Error al actualizar el usuario");
-      }
+  useEffect(() => {
+    const fetchData = async () => {
+      const userDataResponse = await userData();
+      const usuarioCompleto = await fetchUserData(userDataResponse.id);
+      setUsser(usuarioCompleto);
     };
 
-  const handleFirstActive = (user)  => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    handleFirstActive(usser);
+  }, [usser]);
+
+  async function fetchUserData(user) {
+    try {
+      const userDataApi = await fetchApi<User>({
+        endpoint: "users/" + user,
+      });
+
+      return userDataApi;
+    } catch (error) {
+      console.log("error", error);
+    }
+  }
+
+  const editStyleUsuario = async (dark: boolean, neu: boolean) => {
+    const usuarioUpdated = { ...usser!, darkmode: dark, neumorphismmode: neu };
+    // console.log(usuarioUpdated);
+    try {
+      const res = await editarUsuario(usuarioUpdated.id, usuarioUpdated);
+      storeUser(res);
+      console.log(res);
+      if (!res.status) {
+        setUsser(usuarioUpdated);
+      } else {
+        toast.error(res);
+        console.log(res);
+      }
+    } catch (error) {
+      toast.error("Error al actualizar el usuario");
+    }
+  };
+
+  const handleFirstActive = (user) => {
     var btnDark = document.getElementById("btnDark")!;
     var btnLight = document.getElementById("btnLight")!;
     var btnNeu = document.getElementById("btnNeu")!;
@@ -81,78 +82,197 @@ const TopBar = () => {
       setLight(true);
       btnLight.click();
     }
-  }
+  };
 
-    const handleNeumorphismMode = () => {
-
-      if (neumorphism === false) {
-        setLight(false);
-        setDark(false);
-        setNeumorphism(true);
-        editStyleUsuario(false, true);
-      }
-
-      document.body.classList.remove("dark");
-      document.body.classList.remove("light");
-      document.body.classList.add("neumorphism");
-
-      console.log("Neumorfismo Activado");
+  const handleNeumorphismMode = () => {
+    if (neumorphism === false) {
+      setLight(false);
+      setDark(false);
+      setNeumorphism(true);
+      editStyleUsuario(false, true);
     }
 
-    const handleDarkMode = () => {
+    document.body.classList.remove("dark");
+    document.body.classList.remove("light");
+    document.body.classList.add("neumorphism");
 
-      if (dark === false) {
-        setLight(false);
-        setNeumorphism(false);
-        setDark(true);
-        editStyleUsuario(true, false);
-      }
+    console.log("Neumorfismo Activado");
+  };
 
-      document.body.classList.remove("neumorphism");
-      document.body.classList.remove("light");
-      document.body.classList.add("dark");
-      
-      console.log("Dark Mode Activado");
-    };
+  const handleDarkMode = () => {
+    if (dark === false) {
+      setLight(false);
+      setNeumorphism(false);
+      setDark(true);
+      editStyleUsuario(true, false);
+    }
 
-    const handleLightMode = () => {
+    document.body.classList.remove("neumorphism");
+    document.body.classList.remove("light");
+    document.body.classList.add("dark");
 
-      if (light === false) {
-        setDark(false);
-        setNeumorphism(false);
-        setLight(true);
-        editStyleUsuario(false, false);
-      }
-      
+    console.log("Dark Mode Activado");
+  };
 
-      document.body.classList.remove("dark");
-      document.body.classList.remove("neumorphism");
-      document.body.classList.add("light");
+  const handleLightMode = () => {
+    if (light === false) {
+      setDark(false);
+      setNeumorphism(false);
+      setLight(true);
+      editStyleUsuario(false, false);
+    }
 
-      console.log("Modo Light Activado");
-    };
+    document.body.classList.remove("dark");
+    document.body.classList.remove("neumorphism");
+    document.body.classList.add("light");
 
-    return(
-        <section className="topbar w-full h-16 mb-12 flex items-center justify-between">
-            <form className={`topbar__searchbar `}>
-                <input type="text" placeholder="Buscar..." className="topbar__search-input"/>
-                <button type="submit" className={`topbar__search-button `}>
-                <i className="fa-solid fa-magnifying-glass text-base"></i>
-                </button>
-            </form>
-            <div className="topbar__side-buttons">
-                <button id="btnLight" onClick={handleLightMode} className={`topbar__button ${light ? "active": ""}`}>
-                <i id="sunIcon" className={`${light ? "fa-solid": "fa-regular"}  fa-sun`}></i>
-                </button>
-                <button id="btnDark" onClick={handleDarkMode} className={`topbar__button ${dark  ? "active": ""} `}>
-                <i id="moonIcon" className={`${dark ? "fa-solid": "fa-regular"}  fa-moon`}></i>
-                </button>
-                <button id="btnNeu" onClick={handleNeumorphismMode} className={`topbar__button ${neumorphism  ? "active": ""} `}>
-                <i id="eyeIcon" className={`${neumorphism ? "fa-solid": "fa-regular"}  fa-eye`}></i>
-                </button>
-            </div>
-        </section>
-    )
-}
+    console.log("Modo Light Activado");
+  };
+
+  return (
+    <section className="topbar w-full h-16 mb-12 flex items-center justify-between">
+      <form className={`topbar__searchbar`}>
+        <input
+          type="text"
+          placeholder="Buscar..."
+          className="topbar__search-input"
+        />
+        <button type="submit" className={`topbar__search-button `}>
+          <i className="fa-solid fa-magnifying-glass text-base"></i>
+        </button>
+      </form>
+      <div className="topbar__side-buttons">
+        <button
+          id="btnLight"
+          onClick={handleLightMode}
+          className={`topbar__button ${light ? "active" : ""}`}
+        >
+          <i
+            id="sunIcon"
+            className={`${light ? "fa-solid" : "fa-regular"}  fa-sun`}
+          ></i>
+        </button>
+        <button
+          id="btnDark"
+          onClick={handleDarkMode}
+          className={`topbar__button ${dark ? "active" : ""} `}
+        >
+          <i
+            id="moonIcon"
+            className={`${dark ? "fa-solid" : "fa-regular"}  fa-moon`}
+          ></i>
+        </button>
+        <button
+          id="btnNeu"
+          onClick={handleNeumorphismMode}
+          className={`topbar__button ${neumorphism ? "active" : ""} `}
+        >
+          <i
+            id="eyeIcon"
+            className={`${neumorphism ? "fa-solid" : "fa-regular"}  fa-eye`}
+          ></i>
+        </button>
+      </div>
+      <div className="topbar-responsive__image sm:hidden">
+        <img src="/icono.png" alt="" className="sidebar__logo max-w-[2.5rem]" />
+      </div>
+      <button
+        className="topbar__toggler md:hidden flex hover:text-white active:fill-white hover:fill-white hover:stroke-white cursor-pointer border rounded-md hover:bg-customPrimary p-3 transition-all duration-300"
+        type="button"
+        aria-expanded="false"
+        aria-label="Navegación"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <span className="topbar__toggler-default">
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <line
+              x1="3.5"
+              y1="5.5"
+              x2="21.5"
+              y2="5.5"
+              stroke="#292D32"
+              strokeWidth="3"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="line"
+            />
+            <line
+              x1="4.5"
+              y1="12.5"
+              x2="21.5"
+              y2="12.5"
+              stroke="#292D32"
+              strokeWidth="3"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="line"
+            />
+            <line
+              x1="11.5"
+              y1="19.5"
+              x2="21.5"
+              y2="19.5"
+              stroke="#292D32"
+              strokeWidth="3"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="line"
+            />
+          </svg>
+        </span>
+      </button>
+      {isOpen && (
+        <div className="flex flex-col mt-16 lg:hidden absolute top-4 left-0 bg-white z-50 w-full items-center gap-10 pb-10 border-y pt-10">
+          <button
+            id="btnLight"
+            onClick={handleLightMode}
+            className={`topbar__button rounded-xl ${light ? "active" : ""}`}
+          >
+            Modo Default
+            <i
+              id="sunIcon"
+              className={`${light ? "fa-solid" : "fa-regular"} ml-4 fa-sun`}
+            ></i>
+          </button>
+          <button
+            id="btnDark"
+            onClick={handleDarkMode}
+            className={`topbar__button rounded-xl  ${dark ? "active" : ""} `}
+          >
+            Modo Oscuro
+            <i
+              id="moonIcon"
+              className={`${dark ? "fa-solid" : "fa-regular"}  ml-4 fa-moon`}
+            ></i>
+          </button>
+          <button
+            id="btnNeu"
+            onClick={handleNeumorphismMode}
+            className={`topbar__button rounded-xl  ${
+              neumorphism ? "active" : ""
+            } `}
+          >
+            Modo Neumorfismo
+            <i
+              id="eyeIcon"
+              className={`${
+                neumorphism ? "fa-solid" : "fa-regular"
+              }  ml-4 fa-eye`}
+            ></i>
+          </button>
+          <button onClick={logout} className={`w-full`}>
+            Cerrar Sesión
+            <i className="fa fa-right-to-bracket text-2xl mx-auto ml-4"></i>
+          </button>
+        </div>
+      )}
+    </section>
+  );
+};
 
 export default TopBar;
